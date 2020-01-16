a89de38c661d7dacaba05a573b874fb15ba8a88d
Sarthak <Sarthak@DESKTOP-6RPV7R8>
2020-01-16 20:56:36 +0530

    code

---
+++ LAB1/lab1.java
@@ -0,0 +1,16 @@
+public class LAB1
+{ 
+	public static void main(String[] args)
+	{
+		int a =Integer.parseInt(args[0]);
+		int b =Integer.parseInt(args[1]);
+		int c =Integer.parseInt(args[2]);
+		
+		if (a>b && a>c)
+			System.out.println(a + " a is a largest number");
+		if (b>a && b>c)
+			System.out.println(b + " a is a largest number");
+		if (c>b && c>a)
+			System.out.println(c + " a is a largest number");
+	}
+}
